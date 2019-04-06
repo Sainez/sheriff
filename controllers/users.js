a5ecff9d1f410317f04aa381c97b3e72ee853fb4
Sainez Amon <sainezamon@gmail.com>
2019-04-06 14:09:38 +0300

    cors editted

--- controllers/users.js
+++ controllers/users.js
@@ -33,7 +33,15 @@ var checkPassword = function(password){
 };
 
 //setting cors
-app.use(cors());
+
+app.use(cors(), function(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
+    res.send('cors problem fixed:)');
+});
 
 //------------------- Clinician Registration --------------------------------------
 app.post('/regclinician', urlencodedParser, function(req, res){ 
