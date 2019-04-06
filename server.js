4a43da498103df56fcbdecaf261363162b36d8e3
Sainez Amon <sainezamon@gmail.com>
2019-04-06 10:54:27 +0300

    sjdks

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ var corsOptions = {
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 //Get all Routes
 app.get('/*', function(req, res){
